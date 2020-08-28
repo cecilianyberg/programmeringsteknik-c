f9baaf8f3968bb6d3bf61d7cbbb7b94e9722fa02
ELLIESDATOR\cicci <cicci81@hotmail.com>
2020-08-28 15:53:23 +0200

    progress - words

---
+++ words/Words/WordsApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace WordsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Skriv en konsolapplikation som tar emot en skriven text.
+            Console.WriteLine("Enter a string, prefferably ")
+            char [] vowels = new char [] { 'a', 'o', 'i', 'e', 'u', 'y', 'å', 'ä', 'ö' }
+
+            string myTestString = "this is a test";
+            string myLowercaseString = myTestString.ToLower();
+
+            string[] words = myLowercaseString.Split(" "), StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var character in myLowercaseString)
+            {
+                if (vowels.Contains(character))
+                {
+
+                }   
+            }
+
+            for (var i = 0; i < enteredString.LLenght; i++)
+
+            Console.WriteLine("Word coaunt" + wordCount);
+            Console.WriteLine(")
+
+
+            // Vi vill ha ut följande:
+            // Antal ord?
+            // Antal vokaler?
+            // Vilket är det längsta ordet?
+
+            // Word count
+            // Vowel count
+            // Longest word
+
+            
+        }
+    }
+}
